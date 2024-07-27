422615c3eb5adce107cbc53db94cc71a75b54ba5
qbetadev <157713329+qbetadev@users.noreply.github.com>
2024-07-27 12:57:16 +0300

    Add resize and reset buttons

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gridContainer = document.querySelector('.container-grid');
+const buttonSize = document.querySelector('.button-size');
+const buttonReset = document.querySelector('.button-reset');
 
 function createGrid(size) {
     for (let row = 0; row < size; row++) {
-        const row = document.createElement('div');
-        row.classList.add('container-grid__row');
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('container-grid__row');
         for (let cell = 0; cell < size; cell++) {
             const rowCell = document.createElement('div');
             rowCell.classList.add('container-grid__cell');
@@ -11,10 +13,41 @@ function createGrid(size) {
                 rowCell.style.backgroundColor = 'green';
                 rowCell.style.opacity = 1;
             })
-            row.appendChild(rowCell);
+            gridRow.appendChild(rowCell);
         }
-        gridContainer.appendChild(row);
+        gridContainer.appendChild(gridRow);
     }
 }
 
-createGrid(prompt('Enter size'));
+function changeGridSize() {
+    let gridSize = prompt('Please, enter the drawing pad size (max. 100)');
+    if (gridSize > 100) {
+        gridSize = 100;
+    } else if (gridSize <= 0) {
+        gridSize = 1;
+    } else if (!Number(gridSize)) {
+        gridSize = prompt('Enter number!');
+    }
+    gridContainer.textContent = '';
+    createGrid(gridSize);
+    return gridSize;
+}
+
+function resetGrid() {
+    gridContainer.textContent = '';
+    createGrid(userGridSize);
+}
+
+let defaultGridSize = 16;
+let userGridSize;
+
+buttonSize.addEventListener('click', () => {
+    userGridSize = changeGridSize();
+    console.log(userGridSize)
+})
+
+buttonReset.addEventListener('click', () => {
+    resetGrid();
+})
+
+createGrid(defaultGridSize);
