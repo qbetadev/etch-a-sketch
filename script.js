6e724d3cd9e3f9ae63cd44edd5f235c4833e89c9
qbetadev <157713329+qbetadev@users.noreply.github.com>
2024-07-28 14:08:42 +0300

    Add random color painting

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector('.container-grid');
-const buttonSize = document.querySelector('.container-buttons__button-size');
-const buttonReset = document.querySelector('.container-buttons__button-reset');
+const buttonSize = document.querySelector('.button_size');
+const buttonReset = document.querySelector('.button_reset');
 
 function createGrid(size) {
     for (let row = 0; row < size; row++) {
@@ -10,14 +10,42 @@ function createGrid(size) {
             const rowCell = document.createElement('div');
             rowCell.classList.add('container-grid__cell');
             rowCell.addEventListener('mouseover', () => {
-                rowCell.style.backgroundColor = 'green';
-                rowCell.style.opacity = 1;
+                randomColor(rowCell);
+                increaseOpacity(rowCell);
+            })
+            rowCell.addEventListener('mousedown', () => {
+                randomColor(rowCell);
+                increaseOpacity(rowCell);
             })
             gridRow.appendChild(rowCell);
         }
         gridContainer.appendChild(gridRow);
     }
 }
+
+//Random number for color generator
+function randomNumber(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+//Random color generator
+function randomColor(element) {
+	element.style.backgroundColor = `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)})`;
+}
+
+//Initial opacity set to 0, increasing step by step by 0.1
+function increaseOpacity(element) {
+	let opacity = Number(element.style.opacity);
+	if (opacity < 1 && opacity >= 0) {
+		opacity += 0.1;
+	} else {
+		opacity = 1;
+	}
+	element.style.opacity = opacity;
+}
+
+//Grid size after each page loading
+let defaultGridSize = 16;
 
 function changeGridSize() {
     let gridSize = prompt('Please, enter the drawing pad size (max. 100)');
@@ -33,6 +61,14 @@ function changeGridSize() {
     return gridSize;
 }
 
+buttonSize.addEventListener('click', () => {
+    userGridSize = changeGridSize();
+})
+
+//Grid size set by user via button
+let userGridSize = 0;
+
+//Reset grid with the last saved size
 function resetGrid() {
     gridContainer.textContent = '';
     if (userGridSize == 0) {
@@ -41,13 +77,6 @@ function resetGrid() {
         createGrid(userGridSize);
     }
 }
-
-let defaultGridSize = 4;
-let userGridSize = 0;
-
-buttonSize.addEventListener('click', () => {
-    userGridSize = changeGridSize();
-})
 
 buttonReset.addEventListener('click', () => {
     resetGrid();
